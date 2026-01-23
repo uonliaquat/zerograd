63750f9bb39fc13c743c2dfc37930c523dde0106
Uon Liaquat <antelopes06@gmail.com>
2026-01-24 04:24:58 +0500

    Updated compile.sh to use compiler optimization flag

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -Iinclude src/main.c src/tokenizer.c src/tensor.c src/dataset.c src/dataloader.c src/utils.c src/layers/embedding.c src/layers/linear.c src/layers/dropout.c src/layers/self_attention_multi_head.c -lm
+gcc -Iinclude src/main.c src/tokenizer.c src/tensor.c src/dataset.c src/dataloader.c src/utils.c src/layers/embedding.c src/layers/linear.c src/layers/dropout.c src/layers/self_attention_multi_head.c -lm -O2
